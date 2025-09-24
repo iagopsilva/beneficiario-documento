ebd4ce2517567d0c274e363c7951798d6f553b5c
iago <iagopsilva073@gmail.com>
2025-09-24 15:54:44 -0300

    implementa main. Teste Okay!

--- src/main/java/br/com/iago/beneficiario_documento/BeneficiarioDocumentoApplication.java
+++ src/main/java/br/com/iago/beneficiario_documento/BeneficiarioDocumentoApplication.java
@@ -2,12 +2,23 @@ package br.com.iago.beneficiario_documento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class BeneficiarioDocumentoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeneficiarioDocumentoApplication.class, args);
-	}
+    @GetMapping
+    public String getHomeTeste() {
+        return "Beneficiario API Home";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BeneficiarioDocumentoApplication.class, args);
+    }
 
 }
