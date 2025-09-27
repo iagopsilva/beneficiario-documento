1ca285635110571ac19ed3daae0c87d408174721
iago <iagopsilva073@gmail.com>
2025-09-27 04:50:06 -0300

    implementa post documento do beneficiario

--- src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoBeneficiarioListResponse.java
+++ src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoBeneficiarioListResponse.java
@@ -1,11 +1,29 @@
 package br.com.iago.beneficiario_documento.documento.application.api;
 
 import br.com.iago.beneficiario_documento.documento.domain.Documento;
+import lombok.Getter;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Getter
 public class DocumentoBeneficiarioListResponse {
+    private UUID idDocumento;
+    private String tipoDocumento;
+    private String numDocumento;
+    private String descricao;
+
     public static List<DocumentoBeneficiarioListResponse> converte(List<Documento> documentosDoBenficiario) {
-        return null;
+        return documentosDoBenficiario.stream()
+                .map(DocumentoBeneficiarioListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public DocumentoBeneficiarioListResponse(Documento documento) {
+        this.idDocumento = documento.getIdDocumento();
+        this.tipoDocumento = documento.getTipoDocumento();
+        this.numDocumento = documento.getNumDocumento();
+        this.descricao = documento.getDescricao();
     }
 }
