194ff82b3ae5961952357f4216e2eb1a3eba1889
iago <iagopsilva073@gmail.com>
2025-09-27 03:23:33 -0300

    implementa request e responde

--- src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoRequest.java
+++ src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoRequest.java
@@ -1,4 +1,17 @@
 package br.com.iago.beneficiario_documento.documento.application.api;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Value
 public class DocumentoRequest {
+    @NotBlank
+    private String tipoDocumento;
+    @NotBlank
+    private String numDocumento;
+    @NotBlank
+    private String descricao;
 }
