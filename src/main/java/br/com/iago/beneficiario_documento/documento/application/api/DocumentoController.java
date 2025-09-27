f066c3088c44cbfa4ec8eaabb687b431cd26be1b
iago <iagopsilva073@gmail.com>
2025-09-27 03:34:08 -0300

    implementa fluxo service. Teste Okay!

--- src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoController.java
+++ src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoController.java
@@ -1,5 +1,7 @@
 package br.com.iago.beneficiario_documento.documento.application.api;
 
+import br.com.iago.beneficiario_documento.documento.application.service.DocumentoService;
+import br.com.iago.beneficiario_documento.documento.domain.Documento;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +11,18 @@ import java.util.UUID;
 @Log4j2
 public class DocumentoController implements DocumentoAPI {
 
+    private final DocumentoService documentoService;
+
+    public DocumentoController(DocumentoService documentoService) {
+        this.documentoService = documentoService;
+    }
+
     @Override
     public DocumentoResponse postDocumento(UUID idBeneficiario, DocumentoRequest documentoRequest) {
         log.info("[inicia] DocumentoController - postDocumento");
         log.info("[idBeneficiario] {}", idBeneficiario);
+        DocumentoResponse documento = documentoService.criaDocumento(idBeneficiario, documentoRequest);
         log.info("[finaliza] DocumentoController - postDocumento");
-        return null;
+        return documento;
     }
 }
