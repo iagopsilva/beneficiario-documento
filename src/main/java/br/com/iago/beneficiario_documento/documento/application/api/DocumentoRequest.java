8db9240994f56aabf3b3c89c17895fcdd4153b2a
iago <iagopsilva073@gmail.com>
2025-09-30 14:30:39 -0300

    implementa swagger

--- src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoRequest.java
+++ src/main/java/br/com/iago/beneficiario_documento/documento/application/api/DocumentoRequest.java
@@ -3,9 +3,6 @@ package br.com.iago.beneficiario_documento.documento.application.api;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Value;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Value
 public class DocumentoRequest {
     @NotBlank
